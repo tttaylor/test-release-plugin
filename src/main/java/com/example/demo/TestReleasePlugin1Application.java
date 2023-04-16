b16dfb486d1f1611b63b627aeb36f9c79a821d73
Tim Taylor <tim@taylorhome.org>
2023-04-16 11:46:38 -0500

    First commit and push

---
+++ src/main/java/com/example/demo/TestReleasePlugin1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestReleasePlugin1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestReleasePlugin1Application.class, args);
+	}
+
+}
